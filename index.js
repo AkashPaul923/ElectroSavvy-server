f0fb4dd53180ba33fa3395ecf3ed237da1037ce0
AkashPaul923 <akashpaul923@gmail.com>
2024-12-23 22:37:59 +0600

    get single service data by id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT ||5000;
@@ -40,6 +40,14 @@ async function run() {
         option = {serviceName : { $regex : search , $options : 'i' }}
       }
       const result = await serviceCollection.find(option).toArray()
+      res.send(result)
+    })
+
+
+    app.get('/service-detail/:id', async (req,res)=>{
+      const id = req.params.id
+      const query = {_id : new ObjectId(id)}
+      const result = await serviceCollection.findOne(query)
       res.send(result)
     })
 
