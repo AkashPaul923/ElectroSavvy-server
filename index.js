f8b5ea49b97502a3cd4f068dc97a5972aeac0d58
AkashPaul923 <akashpaul923@gmail.com>
2024-12-25 01:12:16 +0600

    delete service

--- index.js
+++ index.js
@@ -84,6 +84,13 @@ async function run() {
       res.send(result)
     })
 
+    app.delete("/services/:id", async (req,res)=>{
+      const id = req.params.id
+      const query = {_id : new ObjectId(id)}
+      const result = await serviceCollection.deleteOne(query)
+      res.send(result)
+    })
+
     // Booked Collection APIs
     app.get('/booked-services', async (req, res)=>{
       const {email} = req.query
