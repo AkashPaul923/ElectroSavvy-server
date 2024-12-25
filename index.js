c34d8b1f752b3180dacae30fdc4ca93aefa45897
AkashPaul923 <akashpaul923@gmail.com>
2024-12-25 14:06:40 +0600

    token create and delete api add

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 require('dotenv').config()
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT ||5000;
 
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173/'],
+  credentials: true,
+}));
 app.use(express.json());
+app.use(cookieParser());
+
+
+const verifyToken = ( req, res, next ) => {
+  const token = req.cookies.token
+
+  if( !token ){
+    return res.status(401).send({ message : 'Unauthorized access'})
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, ( err, decoded ) => {
+    if( err ) {
+      return res.status(401).send({ message : 'Unauthorized access'})
+    }
+    req.user = decoded
+    next()
+  })
+}
 
 
 
@@ -31,7 +53,22 @@ async function run() {
     const serviceCollection = client.db('ElectroSavvyDB').collection('Services')
     const bookedServicesCollection = client.db('ElectroSavvyDB').collection('BookedServices')
 
+    // Auth APIs
+    app.post('/jwt', async (req, res)=> {
+      const user = req.body
+      const token = jwt.sign( user, process.env.ACCESS_TOKEN_SECRET, {expiresIn : '1h'})
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: false,
+      }).send({success : true})
+    })
 
+    app.post('/logout', (req, res)=>{
+      res.clearCookie('token', {
+        httpOnly: true,
+        secure: false,
+      }).send({success: true})
+    })
 
     // services APIs
     app.get('/services', async (req, res) => {
@@ -102,6 +139,13 @@ async function run() {
     app.post('/booked-services', async (req, res)=>{
       const newBookedService = req.body
       const result = await bookedServicesCollection.insertOne(newBookedService)
+      res.send(result)
+    })
+
+    app.delete("/booked-services/:id", async (req,res)=>{
+      const id = req.params.id
+      const query = {_id : new ObjectId(id)}
+      const result = await bookedServicesCollection.deleteOne(query)
       res.send(result)
     })
 
